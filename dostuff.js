64e30daeaaf688a58ed6b3eb460b065964ae4757
Bjørn Golberg <s234852@stud.hioa.no>
2018-05-23 19:15:29 +0100

    lok

--- dostuff.js
+++ dostuff.js
@@ -2,16 +2,41 @@
   const content = [
     {
       overskrift: 'Prosjektskisse',
-      pdf: './documents/Bachelor-Prosjektskisse.pdf.'
+      pdf: './documents/Bachelor-Prosjektskisse.pdf'
     },
     {
       overskrift: 'Forprosjekt',
-      pdf: './documents/Forprosjekt.pdf.'
+      pdf: './documents/Forprosjekt.pdf'
+    },
+    {
+      overskrift: 'Prosjektoppgave',
+      pdf: './documents/Prosjektoppgave.pdf'
     }
   ];
-
+  const profiler = [
+    {
+      name: 'Bjørn Golberg',
+      profilbilde: './documents/BjornG.jpg'
+    },
+    {
+      name: 'Thomas Guest',
+      profilbilde: './documents/ThomasG.jpg'
+    },
+    {
+      name: 'Thomas Langseth',
+      profilbilde: './documents/ThomasL.jpg'
+    },
+    {
+      name: 'Kim Knudsen',
+      profilbilde: './documents/KimFK.jpg'
+    },
+    {
+      name: 'Lars Magnus Henriksen',
+      profilbilde: './documents/LarsMH.jpg'
+    }
+  ]
   produserPdfContainers(content);
-
+  produserProfilbilder(profiler);
 
 
   //Metoder:
@@ -20,23 +45,30 @@
 
     const mainElement = document.getElementsByTagName("main")[0];
 
-    contentlist.forEach((content)=>{
+    contentlist.forEach( (content) => {
 
       const pdfContainer = document.createElement("div");
       pdfContainer.className = 'pdfContainer';
-
+      /*
+      const pointerContainer = document.createElement('div')
+      const pointer = document.createElement('i')
+      pointer.className = 'right';
+      pointerContainer.appendChild(pointer)
+      pointerContainer.className = "pointerContainer"
+      */
       const pdfContainerOverskrift = document.createElement("div");
       pdfContainerOverskrift.className += 'pdfContainerOverskrift';
       const overskrift = document.createElement('h1');
       const overskrifttekst = document.createTextNode(content.overskrift);
       overskrift.appendChild(overskrifttekst);
       pdfContainerOverskrift.appendChild(overskrift);
-      pdfContainerOverskrift.addEventListener('click', ()=>{utvidDiv(pdfContainer);});
+      pdfContainerOverskrift.addEventListener('click', () => { utvidDiv(pdfContainer); });
 
       const pdfContainerInnhold = document.createElement("div");
       pdfContainerInnhold.className += 'pdfContainerInnhold';
       PDFObject.embed(content.pdf, pdfContainerInnhold);
 
+      //pdfContainerOverskrift.appendChild(pointerContainer)
       pdfContainer.appendChild(pdfContainerOverskrift);
       pdfContainer.appendChild(pdfContainerInnhold);
 
@@ -55,4 +87,29 @@
     else
       element.style.height = '0px';
 
+  }
+
+  function produserProfilbilder(profiler) {
+    const footerElement = document.getElementsByTagName("footer")[0]
+    const profilerContainer = document.createElement('div')
+    profilerContainer.id = 'profilerContainer'
+
+    profiler.forEach( (profil) => {
+      const profilContainer = document.createElement('div')
+      profilContainer.className = 'profilContainer'
+
+      const image = document.createElement("img")
+      image.src = profil.profilbilde
+      image.className = 'profileBilde'
+
+      const label = document.createElement('div')
+      const labelText = document.createTextNode(profil.name)
+      label.appendChild(labelText)
+
+      profilContainer.appendChild(image)
+      profilContainer.appendChild(label)
+      profilerContainer.appendChild(profilContainer)
+
+    })
+    footerElement.appendChild(profilerContainer)
   }
